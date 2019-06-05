e3db40d9c2c1b19fb6fb9c8eb81193ab39a28b2d
zhakui <zh-ge@163.com>
2019-06-05 20:07:44 +0800

    Update 4 and 10.

--- mini-parser/src/com/zhkui/miniparser/miniparser.java
+++ mini-parser/src/com/zhkui/miniparser/miniparser.java
@@ -6,24 +6,24 @@ package com.zhkui.miniparser;
 public class miniparser {
     public static void main(String[] args) {
         String s = "abcdefgh";
-        System.out.print(isPalindrome(12231));
+        System.out.print(isMatch("aaa","a*a"));
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(0);
-        while (l1.next != null && l2.next != null) {
-            ListNode midResult;
-            if (l1.val + l2.val > 10) {
-                midResult = new ListNode((l1.val + l2.val) / 10);
-            } else {
-                midResult = new ListNode(l1.val + l2.val);
-            }
-            result.next = midResult;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        return result.next;
-    }
+    // public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    //     ListNode result = new ListNode(0);
+    //     while (l1.next != null && l2.next != null) {
+    //         ListNode midResult;
+    //         if (l1.val + l2.val > 10) {
+    //             midResult = new ListNode((l1.val + l2.val) / 10);
+    //         } else {
+    //             midResult = new ListNode(l1.val + l2.val);
+    //         }
+    //         result.next = midResult;
+    //         l1 = l1.next;
+    //         l2 = l2.next;
+    //     }
+    //     return result.next;
+    // }
 
     public static String longestPalindrome1(String s) {
         String result = "";
@@ -202,6 +202,86 @@ public class miniparser {
             base /= 100;
         }
         return true;
+    }
+
+    //Median of Two Sorted Arrays
+    //There are two sorted arrays nums1 and nums2 of size m and n respectively.
+    //Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+    //You may assume nums1 and nums2 cannot be both empty.
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] mergeArr = merge(nums1,nums2);
+        double median = 0.0;
+        if (mergeArr.length%2 == 0){
+            median = (mergeArr[mergeArr.length/2] + mergeArr[mergeArr.length/2 - 1])/2.0;
+        }else{
+            median = mergeArr[mergeArr.length/2];
+        }    
+        return median;
+    }
+    
+    public int[] merge(int[]  right,int[] left){
+        int[] result = new int[right.length+left.length];
+        for(int index=0, i=0, j=0; index < result.length;index++){
+            if(i >= right.length && j < left.length){
+                result[index] = left[j++];
+            }
+            else if(i < right.length && j >= left.length){
+                result[index] = right[i++];
+            }
+            else if(right[i] < left[j]){
+                result[index] = right[i++];
+            }
+            else{
+                result[index] = left[j++];
+            }
+        }
+        return result;
+    }
+
+    //10. Regular Expression Matching
+    public static boolean isMatch(String s, String p) {
+        if(p.isEmpty()) return s.isEmpty();
+        boolean match = !s.isEmpty() && (s.charAt(0)==p.charAt(0) || p.charAt(0) == '.');
+        if (p.length() > 1 && p.charAt(1) == '*'){
+            return (match && isMatch(s.substring(1), p)) || isMatch(s, p.substring(2));
+        }else{
+            return match && isMatch(s.substring(1),p.substring(1));
+        }
+    }
+
+    
+    public boolean isMatch(String s, String p) {
+        
+        char[] text = s.toCharArray();
+        char[] pattern = p.toCharArray();
+        boolean T[][] = new boolean[text.length + 1][pattern.length + 1];
+
+        T[0][0] = true;
+        //Deals with patterns like a* or a*b* or a*b*c*
+        for (int i = 1; i < pattern.length + 1; i++) {
+            if (pattern[i-1] == '*') {
+                T[0][i] = T[0][i - 2];
+            }
+        }
+
+        for (int i = 1; i < text.length + 1; i++) {
+            for (int j = 1; j < pattern.length + 1; j++) {
+                if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
+                    T[i][j] = T[i-1][j-1];
+                } else if (pattern[j - 1] == '*')  {
+                    // consider zero occurence first
+                    T[i][j] = T[i][j - 2];
+                    // check if the previous char in pattern is either '.' or same char as in the string
+                    if (pattern[j-2] == '.' || pattern[j - 2] == text[i - 1]) {
+                        T[i][j] = T[i][j] | T[i - 1][j];
+                    }
+                } else {
+                    T[i][j] = false;
+                }
+            }
+        }
+        return T[text.length][pattern.length];
+        
     }
 }
 
